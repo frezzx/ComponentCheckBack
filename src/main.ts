fd78fffe14d1544b7a51f8994b367dee6c59abec
estevao <pietro01patricia@gmail.com>
2025-06-04 10:15:23 -0400

    fix: fix port

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001',
-    credentials: true,
+    origin: true,
   });
+  console.log('porta ', 3333)
 
-  const port = process.env.PORT ?? 3000;
+  const port = 3333;
 
   await app.listen(port);
   console.log(`🚀 Server running at http://localhost:${port}`);
